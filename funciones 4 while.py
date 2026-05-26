e275fa2446bc8ffc9174e8c86fe1514f8dc21a0c
Rodnyn Jesus <rjtarazonas97@gmail.com>
2026-05-25 22:53:39 -0500

    Clase 8 de python

---
+++ funciones 4 while.py
@@ -0,0 +1,54 @@
+# Mi primera Calculadora en python
+
+
+def sumar(numero1, numero2):
+    resultado = numero1 + numero2
+    print(f"La suma entre {numero1} y {numero2} es igual a: {resultado}")
+
+
+def restar(numero1, numero2):
+    resultado = numero1 - numero2
+    print(f"La resta entre {numero1} y {numero2} es igual a: {resultado}")
+
+
+def multiplicar(numero1, numero2):
+    resultado = numero1 * numero2
+    print(f"La multiplicacion entre {numero1} y {numero2} es igual a: {resultado}")
+
+
+def dividir(numero1, numero2):
+    resultado = numero1 / numero2
+    print(f"La division entre {numero1} y {numero2} es igual a: {resultado}")
+
+
+continuar = True
+
+
+while continuar:
+    primer_numero = int(input("Ingrese el primer numero: "))
+    segundo_numero = int(input("Ingrese el segundo numero: "))
+    operacion = input("Ingrese sel tipo de operacion a realizar: ")
+
+    if operacion == "sumar":
+        sumar(primer_numero, segundo_numero)
+    elif operacion == "restar":
+        restar(primer_numero, segundo_numero)
+    elif operacion == "multiplicar":
+        multiplicar(primer_numero, segundo_numero)
+    elif operacion == "dividir":
+        dividir(primer_numero, segundo_numero)
+    else:
+        print(
+            "Por favor ingrese una operacion valida"\
+                "sumar, restar, multiplicar o dividir"
+        )
+
+    desea_continuar = input("Desea realizar otra operacion y/n:  ")
+    
+    if desea_continuar == 'y':
+        continuar = True
+    elif desea_continuar == 'n':
+        continuar = False
+    else:
+        print("Seleccion no valida")
+        break
