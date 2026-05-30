ca3727fefbc298363730f5d30e9cab114a4e8dd8
Rodnyn Jesus <rjtarazonas97@gmail.com>
2026-05-29 23:53:15 -0500

    Clase 11 de python

---
+++ return.py
@@ -0,0 +1,6 @@
+def sumar(numero1,numero2):
+    resultado = numero1 + numero2
+    print(f"La suma entre {numero1} y {numero2} es: {resultado}")
+    
+resultado = sumar(3,3)
+print(f"El resultado es {resultado}")
